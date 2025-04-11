9e542525fd96798c6e67b040efd36cc3652eaa38
nishanth123kgr <nishanth123kgr@gmail.com>
2025-04-11 17:32:12 +0530

    Add Winston logger and update logging mechanism in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const logger = require('./utils/logger');
 
 require('dotenv').config();
 
@@ -14,4 +15,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/payment', paymentRoute);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`)
+});
--- utils/logger.js
+++ utils/logger.js
@@ -1,5 +1,19 @@
-const pino = require('pino')
-const logger = pino({level: process.env.LOG_LEVEL || 'info'})
+// logger.js
+const { createLogger, format, transports } = require('winston');
+const path = require('path');
 
+const logger = createLogger({
+  level: 'info', // log levels: error, warn, info, http, verbose, debug, silly
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level.toUpperCase()}] ${message}`;
+    })
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: path.join(__dirname, 'logs', 'app.log') })
+  ]
+});
 
-module.exports = logger
+module.exports = logger;
