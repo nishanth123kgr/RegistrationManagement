5f7e1834c6bcfa7ab74bd08b90d85a1e624d4996
Sharon Joseph <90709670+josephsharon07@users.noreply.github.com>
2025-04-14 18:35:50 +0530

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -17,7 +17,7 @@ const htmlContent = fs.readFileSync(path.join(__dirname, 'mail.html'), 'utf-8');
 // Define the email options
 const mailOptions = {
   from: 'techblitz@bluecape.site',
-  to: 'nishanth123kgr@gmail.com',
+  to: 'josephsharon051@gmail.com',
   subject: 'TechBlitz 2025 - Registration Confirmation',
   html: htmlContent,
 };
